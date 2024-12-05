18669bbad211028a1f2428d3f9ae561413d62be3
Dimitri <dimitridumonet@gmail.com>
2024-12-05 21:36:18 +0700

    chore: bump sdk version

--- src/shortest-example.ts
+++ src/shortest-example.ts
@@ -1,8 +1,8 @@
-import zkeSDK from "@zk-email/sdk";
-import fs from "fs/promises";
+import zkeSDK from '@zk-email/sdk';
+import fs from 'fs/promises';
 
 // Copy slug from UI homepage
-const blueprintSlug = "Bisht13/SuccinctZKResidencyInvite@v1"
+const blueprintSlug = 'Bisht13/SuccinctZKResidencyInvite@v1';
 
 async function main() {
   const sdk = zkeSDK();
@@ -14,13 +14,13 @@ async function main() {
   const prover = blueprint.createProver();
 
   // Get eml
-  const eml = (await fs.readFile("emls/residency.eml")).toString();
+  const eml = (await fs.readFile('emls/residency.eml')).toString();
 
   // Generate and wait until proof is generated, can take up to a few minutes
   const proof = await prover.generateProof(eml);
   const { proofData, publicData } = proof.getProofData();
-  console.log("proof: ", proofData);
-  console.log("public: ", publicData);
+  console.log('proof: ', proofData);
+  console.log('public: ', publicData);
 }
 
 main();
