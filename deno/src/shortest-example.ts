94ffbbcc6f73ee3e49c4a93e78cbf8311a2806d2
Leon <leondo123@gmail.com>
2025-09-29 23:46:09 -0400

    deno bump zk-email/sdk to 2.0.10

--- deno/src/shortest-example.ts
+++ deno/src/shortest-example.ts
@@ -1,11 +1,11 @@
-import zkeSDK from '@zk-email/sdk';
-import fs from 'node:fs/promises';
+import { initZkEmailSdk } from "@zk-email/sdk";
+import fs from "node:fs/promises";
 
 // Copy slug from UI homepage
-const blueprintSlug = 'Bisht13/SuccinctZKResidencyInvite@v2';
+const blueprintSlug = "Bisht13/SuccinctZKResidencyInvite@v2";
 
 async function main() {
-  const sdk = zkeSDK();
+  const sdk = initZkEmailSdk();
 
   // Get an instance of Blueprint
   const blueprint = await sdk.getBlueprint(blueprintSlug);
@@ -14,13 +14,13 @@ async function main() {
   const prover = blueprint.createProver();
 
   // Get eml
-  const eml = (await fs.readFile('../emls/residency.eml')).toString();
+  const eml = (await fs.readFile("../emls/residency.eml")).toString();
 
   // Generate and wait until proof is generated, can take up to a few minutes
   const proof = await prover.generateProof(eml);
   const { proofData, publicData } = proof.getProofData();
-  console.log('proof: ', proofData);
-  console.log('public: ', publicData);
+  console.log("proof: ", proofData);
+  console.log("public: ", publicData);
 }
 
 main();
